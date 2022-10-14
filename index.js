a2afe125d4d81cfffc3f9d9a1f1c9a8a04a55217
Steven Henke <steven@foundrymakes.com>
2022-10-14 12:35:13 -0500

    update to current

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 $(document).ready(function () {
+  const homeLink = $("#home-link")
+  const ctaLink = $("#contest-link")
+  const galleryLink = $("#gallery-link")
+
+  homeLink.addClass("active");
+
   $("#right-gallery-chevron").on("click", () => {
     var leftPos = $('div.gallery-section').scrollLeft();
     $("div.gallery-section").animate({
@@ -12,5 +18,40 @@ $(document).ready(function () {
     }, 400);
   })
 
-})
+  ctaLink.on("click", () => {
+    galleryLink.removeClass("active")
+    homeLink.removeClass("active")
+    ctaLink.addClass("active")
+  })
+
+  galleryLink.on("click", () => {
+    ctaLink.removeClass("active")
+    homeLink.removeClass("active")
+    galleryLink.addClass("active")
+  })
+  homeLink.on("click", () => {
+    ctaLink.removeClass("active")
+    galleryLink.removeClass("active")
+    homeLink.addClass("active")
+  })
+
+  window.onscroll = () => {
+    if (scrollY <= 545) {
+      ctaLink.removeClass("active")
+      galleryLink.removeClass("active")
+      homeLink.addClass("active")
+    }
+    if (545 < scrollY && scrollY <= 965) {
+      ctaLink.addClass("active")
+      galleryLink.removeClass("active")
+      homeLink.removeClass("active")
+    }
+    if (scrollY > 965) {
+      ctaLink.removeClass("active")
+      galleryLink.addClass("active")
+      homeLink.removeClass("active")
+    }
+  }
+}
+)
 
